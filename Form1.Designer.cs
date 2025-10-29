2b7c5ae3e79d3be9b080a8978ec10068b349ce04
Petr <petrben229@gmail.com>
2025-10-29 19:32:53 +0100

    První commit - základ hry Člověče nezlob se

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,299 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnHodKostkou = new System.Windows.Forms.Button();
+            this.lblCisloKostky = new System.Windows.Forms.Label();
+            this.panelHraciPole = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.panelHraciPole.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnHodKostkou
+            // 
+            this.btnHodKostkou.Location = new System.Drawing.Point(12, 12);
+            this.btnHodKostkou.Name = "btnHodKostkou";
+            this.btnHodKostkou.Size = new System.Drawing.Size(115, 49);
+            this.btnHodKostkou.TabIndex = 0;
+            this.btnHodKostkou.Text = "Kostka";
+            this.btnHodKostkou.UseVisualStyleBackColor = true;
+            this.btnHodKostkou.Click += new System.EventHandler(this.btnHodKostkou_Click);
+            // 
+            // lblCisloKostky
+            // 
+            this.lblCisloKostky.AutoSize = true;
+            this.lblCisloKostky.Location = new System.Drawing.Point(155, 28);
+            this.lblCisloKostky.Name = "lblCisloKostky";
+            this.lblCisloKostky.Size = new System.Drawing.Size(0, 16);
+            this.lblCisloKostky.TabIndex = 1;
+            // 
+            // panelHraciPole
+            // 
+            this.panelHraciPole.Controls.Add(this.label20);
+            this.panelHraciPole.Controls.Add(this.label19);
+            this.panelHraciPole.Controls.Add(this.label18);
+            this.panelHraciPole.Controls.Add(this.label17);
+            this.panelHraciPole.Controls.Add(this.label16);
+            this.panelHraciPole.Controls.Add(this.label15);
+            this.panelHraciPole.Controls.Add(this.label14);
+            this.panelHraciPole.Controls.Add(this.label13);
+            this.panelHraciPole.Controls.Add(this.label12);
+            this.panelHraciPole.Controls.Add(this.label11);
+            this.panelHraciPole.Controls.Add(this.label10);
+            this.panelHraciPole.Controls.Add(this.label9);
+            this.panelHraciPole.Controls.Add(this.label8);
+            this.panelHraciPole.Controls.Add(this.label7);
+            this.panelHraciPole.Controls.Add(this.label6);
+            this.panelHraciPole.Controls.Add(this.label5);
+            this.panelHraciPole.Controls.Add(this.label4);
+            this.panelHraciPole.Controls.Add(this.label3);
+            this.panelHraciPole.Controls.Add(this.label2);
+            this.panelHraciPole.Controls.Add(this.label1);
+            this.panelHraciPole.Location = new System.Drawing.Point(12, 92);
+            this.panelHraciPole.Name = "panelHraciPole";
+            this.panelHraciPole.Size = new System.Drawing.Size(767, 346);
+            this.panelHraciPole.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(124, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(174, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(224, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(274, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(324, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(374, 31);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 16);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(424, 31);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 16);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(474, 31);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(51, 16);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "label10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(531, 31);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(51, 16);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(588, 31);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(51, 16);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "label12";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(645, 31);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(51, 16);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "label13";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(702, 31);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(51, 16);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "label14";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(702, 66);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(51, 16);
+            this.label15.TabIndex = 14;
+            this.label15.Text = "label15";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(702, 108);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(51, 16);
+            this.label16.TabIndex = 15;
+            this.label16.Text = "label16";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(702, 147);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(51, 16);
+            this.label17.TabIndex = 16;
+            this.label17.Text = "label17";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(702, 187);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(51, 16);
+            this.label18.TabIndex = 17;
+            this.label18.Text = "label18";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(702, 228);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(51, 16);
+            this.label19.TabIndex = 18;
+            this.label19.Text = "label19";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(702, 259);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(51, 16);
+            this.label20.TabIndex = 19;
+            this.label20.Text = "label20";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelHraciPole);
+            this.Controls.Add(this.lblCisloKostky);
+            this.Controls.Add(this.btnHodKostkou);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panelHraciPole.ResumeLayout(false);
+            this.panelHraciPole.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnHodKostkou;
+        private System.Windows.Forms.Label lblCisloKostky;
+        private System.Windows.Forms.Panel panelHraciPole;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,58 @@ namespace Hra_člověče_nezlob_se
 {
     public partial class Form1 : Form
     {
+        private List<Label> policka = new List<Label>();
+        private int poziceHrace = -1; // -1 = ještě není na poli
+        private Random nahoda = new Random();
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void VytvorHraciPole()
+        {
+            // Vytvoří 20 polí v řadě
+            int pocetPolicek = 20;
+            int velikost = 30;
+
+            for (int i = 0; i < pocetPolicek; i++)
+            {
+                Label pole = new Label();
+                pole.Size = new Size(velikost, velikost);
+                pole.Location = new Point(10 + i * (velikost + 5), 50);
+                pole.BorderStyle = BorderStyle.FixedSingle;
+                pole.BackColor = Color.White;
+                panelHraciPole.Controls.Add(pole);
+                policka.Add(pole);
+            }
+        }
+
+        private void btnHodKostkou_Click(object sender, EventArgs e)
+        {
+            int hod = nahoda.Next(1, 7);
+            lblCisloKostky.Text = $"Padlo: {hod}";
+
+            if (poziceHrace == -1)
+            {
+                // figurka nastupuje na start
+                poziceHrace = 0;
+                policka[0].BackColor = Color.Red;
+            }
+            else
+            {
+                // pohyb figurky
+                int novaPozice = poziceHrace + hod;
+
+                if (novaPozice >= policka.Count)
+                    novaPozice = policka.Count - 1; // konec
+
+                policka[poziceHrace].BackColor = Color.White; // staré políčko
+                policka[novaPozice].BackColor = Color.Red;    // nové políčko
+                poziceHrace = novaPozice;
+            }
+        }
     }
+
 }
